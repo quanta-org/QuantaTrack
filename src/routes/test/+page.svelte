a63c38b4187ab2eb3a725828ae9c891048eae99c
Trevor <trevor@quanta-inc.com>
2023-02-01 16:22:16 -0500

    temp-fix: more options

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -21,73 +21,189 @@
 </script>
 
 <h1 class="text-white text-4xl font-bold mt-5">Keydown</h1>
-<div class="text-white flex h-96 flex-wrap bg-gray-900 h-20">
-	{#each keydown as event}
-		<div class="bg-red-900 p-2">
-			{event.key}
-		</div>
-		<div class="bg-green-900 p-2">
-			{event.altKey}
-		</div>
-		<div class="bg-blue-900 p-2">
-			{event.ctrlKey}
-		</div>
-        <div class="bg-pink-900 p-2">
-			{event.metaKey}
-		</div>
-        <div class="bg-purple-900 p-2">
-			{event.shiftKey}
-		</div>
-        <div class="bg-yellow-900 p-2">
-			{event.keyCode}
-		</div>
-	{/each}
+<div class="flex overflow-x-auto">
+    <table class="text-white bg-gray-900 overflow-x-auto">
+        <tr>
+            <th>Key</th>
+            <th>altKey</th>
+            <th>cthlKey</th>
+            <th>metaKey</th>
+            <th>shiftKey</th>
+            <th>keyCode</th>
+            <th>charCode</th>
+            <th>location</th>
+            <th>bubbles</th>
+            <th>code</th>
+            <th>composed</th>
+            <th>detail</th>
+            <th>DOM left</th>
+            <th>DOM right</th>
+            <th>DOM standard</th>
+            <th>DOM numpad</th>
+            <th>Target</th>
+            <th>isTrusted</th>
+            <th>view</th>
+            <th>Return value</th>
+            <th>Cancellable</th>
+            <th>Type</th>
+            <th>At target</th>
+            <th>None?</th>
+            <th>Src element</th>
+        </tr>
+        {#each keydown as event}
+            <tr>
+                <td>{event.key}</td>
+                <td>{event.altKey}</td>
+                <td>{event.ctrlKey}</td>
+                <td>{event.metaKey}</td>
+                <td>{event.shiftKey}</td>
+                <td>{event.keyCode}</td>
+                <td>{event.charCode}</td>
+                <td>{event.location}</td>
+                <td>{event.bubbles}</td>
+                <td>{event.code}</td>
+                <td>{event.composed}</td>
+                <td>{event.detail}</td>
+                <td>{event.DOM_KEY_LOCATION_LEFT}</td>
+                <td>{event.DOM_KEY_LOCATION_RIGHT}</td>
+                <td>{event.DOM_KEY_LOCATION_STANDARD}</td>
+                <td>{event.DOM_KEY_LOCATION_NUMPAD}</td>
+                <td>{event.currentTarget}</td>
+                <td>{event.isTrusted}</td>
+                <td>{event.view}</td>
+                <td>{event.returnValue}</td>
+                <td>{event.cancelable}</td>
+                <td>{event.type}</td>
+                <td>{event.AT_TARGET}</td>
+                <td>{event.NONE}</td>
+                <td>{event.srcElement}</td>
+            </tr>
+        {/each}
+    </table>
 </div>
-<h1 class="text-white text-4xl font-bold mt-5">Keypress</h1>
-<div class="text-white flex h-96 flex-wrap bg-gray-900 h-20">
-	{#each keypress as event}
-		<div class="bg-red-900 p-2">
-			{event.key}
-		</div>
-		<div class="bg-green-900 p-2">
-			{event.altKey}
-		</div>
-        <div class="bg-blue-900 p-2">
-			{event.ctrlKey}
-		</div>
-        <div class="bg-pink-900 p-2">
-			{event.metaKey}
-		</div>
-        <div class="bg-purple-900 p-2">
-			{event.shiftKey}
-		</div>
-        <div class="bg-yellow-900 p-2">
-			{event.keyCode}
-		</div>
-	{/each}
-</div>
+
 <h1 class="text-white text-4xl font-bold mt-5">Keyup</h1>
-<div class="text-white flex h-96 flex-wrap bg-gray-900 h-20">
-	{#each keyup as event}
-		<div class="bg-red-900 p-2">
-			{event.key}
-		</div>
-		<div class="bg-green-900 p-2">
-			{event.altKey}
-		</div>
-        <div class="bg-blue-900 p-2">
-			{event.ctrlKey}
-		</div>
-        <div class="bg-pink-900 p-2">
-			{event.metaKey}
-		</div>
-        <div class="bg-purple-900 p-2">
-			{event.shiftKey}
-		</div>
-        <div class="bg-yellow-900 p-2">
-			{event.keyCode}
-		</div>
-	{/each}
+<div class="flex overflow-x-auto">
+    <table class="text-white bg-gray-900 overflow-x-auto border">
+        <tr>
+            <th>Key</th>
+            <th>altKey</th>
+            <th>cthlKey</th>
+            <th>metaKey</th>
+            <th>shiftKey</th>
+            <th>keyCode</th>
+            <th>charCode</th>
+            <th>location</th>
+            <th>bubbles</th>
+            <th>code</th>
+            <th>composed</th>
+            <th>detail</th>
+            <th>DOM left</th>
+            <th>DOM right</th>
+            <th>DOM standard</th>
+            <th>DOM numpad</th>
+            <th>Target</th>
+            <th>isTrusted</th>
+            <th>view</th>
+            <th>Return value</th>
+            <th>Cancellable</th>
+            <th>Type</th>
+            <th>At target</th>
+            <th>None?</th>
+            <th>Src element</th>
+        </tr>
+        {#each keyup as event}
+            <tr>
+                <td>{event.key}</td>
+                <td>{event.altKey}</td>
+                <td>{event.ctrlKey}</td>
+                <td>{event.metaKey}</td>
+                <td>{event.shiftKey}</td>
+                <td>{event.keyCode}</td>
+                <td>{event.charCode}</td>
+                <td>{event.location}</td>
+                <td>{event.bubbles}</td>
+                <td>{event.code}</td>
+                <td>{event.composed}</td>
+                <td>{event.detail}</td>
+                <td>{event.DOM_KEY_LOCATION_LEFT}</td>
+                <td>{event.DOM_KEY_LOCATION_RIGHT}</td>
+                <td>{event.DOM_KEY_LOCATION_STANDARD}</td>
+                <td>{event.DOM_KEY_LOCATION_NUMPAD}</td>
+                <td>{event.currentTarget}</td>
+                <td>{event.isTrusted}</td>
+                <td>{event.view}</td>
+                <td>{event.returnValue}</td>
+                <td>{event.cancelable}</td>
+                <td>{event.type}</td>
+                <td>{event.AT_TARGET}</td>
+                <td>{event.NONE}</td>
+                <td>{event.srcElement}</td>
+            </tr>
+        {/each}
+    </table>
+</div>
+
+<h1 class="text-white text-4xl font-bold mt-5">Keypress</h1>
+<div class="flex overflow-x-auto">
+    <table class="text-white bg-gray-900 overflow-x-auto">
+        <tr>
+            <th>Key</th>
+            <th>altKey</th>
+            <th>cthlKey</th>
+            <th>metaKey</th>
+            <th>shiftKey</th>
+            <th>keyCode</th>
+            <th>charCode</th>
+            <th>location</th>
+            <th>bubbles</th>
+            <th>code</th>
+            <th>composed</th>
+            <th>detail</th>
+            <th>DOM left</th>
+            <th>DOM right</th>
+            <th>DOM standard</th>
+            <th>DOM numpad</th>
+            <th>Target</th>
+            <th>isTrusted</th>
+            <th>view</th>
+            <th>Return value</th>
+            <th>Cancellable</th>
+            <th>Type</th>
+            <th>At target</th>
+            <th>None?</th>
+            <th>Src element</th>
+        </tr>
+        {#each keypress as event}
+            <tr>
+                <td>{event.key}</td>
+                <td>{event.altKey}</td>
+                <td>{event.ctrlKey}</td>
+                <td>{event.metaKey}</td>
+                <td>{event.shiftKey}</td>
+                <td>{event.keyCode}</td>
+                <td>{event.charCode}</td>
+                <td>{event.location}</td>
+                <td>{event.bubbles}</td>
+                <td>{event.code}</td>
+                <td>{event.composed}</td>
+                <td>{event.detail}</td>
+                <td>{event.DOM_KEY_LOCATION_LEFT}</td>
+                <td>{event.DOM_KEY_LOCATION_RIGHT}</td>
+                <td>{event.DOM_KEY_LOCATION_STANDARD}</td>
+                <td>{event.DOM_KEY_LOCATION_NUMPAD}</td>
+                <td>{event.currentTarget}</td>
+                <td>{event.isTrusted}</td>
+                <td>{event.view}</td>
+                <td>{event.returnValue}</td>
+                <td>{event.cancelable}</td>
+                <td>{event.type}</td>
+                <td>{event.AT_TARGET}</td>
+                <td>{event.NONE}</td>
+                <td>{event.srcElement}</td>
+            </tr>
+        {/each}
+    </table>
 </div>
 
 <svelte:window on:keydown={onKeyDown} on:keypress={onKeyPress} on:keyup={onKeyUp} />
