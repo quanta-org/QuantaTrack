dfa20ab881204549f76837d6a3075222ab6c9361
Trevor <trevor@quanta-inc.com>
2023-02-01 14:42:39 -0500

    unfix: ssl

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA(),
-		basicSsl()
+		SvelteKitPWA()
 	]
 };
 
