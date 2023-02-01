ca0dcf135c9c3dfbbfb3b72ab31e15068e7a28fc
Trevor <trevor@quanta-inc.com>
2023-02-01 16:41:12 -0500

    feat: mobile scanning

--- src/routes/parcels/assembly/+page.svelte
+++ src/routes/parcels/assembly/+page.svelte
@@ -13,35 +13,59 @@
 	let trackingNumberOutbound: string;
 	let client: string;
 	let kitType: string;
-	let trackingNumber: string[] = [""];
-	let kitID: string[] = [""];
+	let trackingNumber: string[] = [''];
+	let kitID: string[] = [''];
 	let isWorkstation: boolean;
 	let isLoading: boolean = false;
+	let isScanning: boolean = false;
 
-	function onKeypress(event: KeyboardEvent) {
+	function onKeydown(event: KeyboardEvent) {
+		// If key length is one, add to array
 		if (event.key.length == 1) {
 			charArray.push(event.key);
+
+			// If the key is tab and the array is long, process scan
 		} else if (event.key == 'Tab' && charArray.length >= 10) {
 			event.preventDefault();
-			let input = charArray.join('');
+			addScan(charArray.join(''));
 			charArray = [];
 
-			if(!trackingNumberOutbound){
-				trackingNumberOutbound = input;
-				return;
-			}
+			// If the key is unidentified, start scanning
+		} else if (event.key == 'Unidentified') {
+			isScanning = true;
+		}
+	}
 
-			for(let i = 0; i < trackingNumber.length; i++){
-				if(trackingNumber[i] == ""){
-					trackingNumber[i] = input;
-					addParcel(i);
-					break;
-				} else if(kitID[i] == ""){
-					kitID[i] = input;
-					break;
-				} else if(i + 1 == trackingNumber.length){
-					addParcel(i);
-				}
+	function onKeypress(event: KeyboardEvent) {
+		if (isScanning) {
+			charArray.push(event.key);
+		}
+	}
+
+	function onKeyup(event: KeyboardEvent) {
+		if (event.key == 'Unidentified' && isScanning) {
+			isScanning = false;
+			addScan(charArray.join(''));
+			charArray = [];
+		}
+	}
+
+	function addScan(input: string) {
+		if (!trackingNumberOutbound) {
+			trackingNumberOutbound = input;
+			return;
+		}
+
+		for (let i = 0; i < trackingNumber.length; i++) {
+			if (trackingNumber[i] == '') {
+				trackingNumber[i] = input;
+				addKit(i);
+				break;
+			} else if (kitID[i] == '') {
+				kitID[i] = input;
+				break;
+			} else if (i + 1 == trackingNumber.length) {
+				addKit(i);
 			}
 		}
 	}
@@ -50,19 +74,19 @@
 		charArray = [];
 	}
 
-	function addParcel(index: number){
-		if (trackingNumber.length <= index + 1){
-			trackingNumber.push("");
-			kitID.push("");
+	function addKit(index: number) {
+		if (trackingNumber.length <= index + 1) {
+			trackingNumber.push('');
+			kitID.push('');
 			trackingNumber = trackingNumber;
 			kitID = kitID;
 		}
 	}
 
-	function deleteParcel(index: number){
-		if(index == trackingNumber.length - 1){
-			trackingNumber[index] = "";
-			kitID[index] = "";
+	function deleteParcel(index: number) {
+		if (index == trackingNumber.length - 1) {
+			trackingNumber[index] = '';
+			kitID[index] = '';
 		} else {
 			trackingNumber.splice(index, 1);
 			kitID.splice(index, 1);
@@ -84,14 +108,19 @@
 	});
 </script>
 
-<svelte:window on:keydown={onKeypress} on:mousemove={clearArray} />
+<svelte:window
+	on:keydown={onKeydown}
+	on:keypress={onKeypress}
+	on:keyup={onKeyup}
+	on:mousemove={clearArray}
+/>
 
 <h1 class="text-4xl font-bold text-white mb-5 flex justify-center">Parcel Assembly</h1>
 {#if !trackingNumberOutbound}
 	<h2 class="text-2xl text-white mb-5 flex justify-center">Scan outbound tracking number</h2>
-{:else if trackingNumber[trackingNumber.length - 1] == ""}
+{:else if trackingNumber[trackingNumber.length - 1] == ''}
 	<h2 class="text-2xl text-white mb-5 flex justify-center">Scan tracking number</h2>
-{:else if kitID[kitID.length - 1] == ""}
+{:else if kitID[kitID.length - 1] == ''}
 	<h2 class="text-2xl text-white mb-5 flex justify-center">Scan kit ID</h2>
 {:else}
 	<h2 class="text-2xl text-white mb-5 flex justify-center">Scan tracking number</h2>
@@ -111,8 +140,8 @@
 					toast.set(
 						JSON.stringify({ message: 'Sucessfully added parcel!', success: 'true', show: 'true' })
 					);
-					trackingNumber = [""];
-					kitID = [""];
+					trackingNumber = [''];
+					kitID = [''];
 				} else if (result.type == 'failure') {
 					toast.set(
 						JSON.stringify({ message: result.data?.message, success: 'false', show: 'true' })
@@ -169,28 +198,14 @@
 			<Label for="client" class="mb-2">
 				<div class="text-white">Clinic Code</div>
 			</Label>
-			<Input
-				type="text"
-				id="client"
-				name="client"
-				tabindex="-1"
-				bind:value={client}
-				required
-			/>
+			<Input type="text" id="client" name="client" tabindex="-1" bind:value={client} required />
 		</div>
 
 		<div class="mb-6">
 			<Label for="kitType" class="mb-2">
 				<div class="text-white">Kit Type</div>
 			</Label>
-			<Input
-				type="text"
-				id="kitType"
-				name="kitType"
-				tabindex="-1"
-				bind:value={kitType}
-				required
-			/>
+			<Input type="text" id="kitType" name="kitType" tabindex="-1" bind:value={kitType} required />
 		</div>
 
 		<div class="mb-6">
@@ -213,8 +228,22 @@
 					<Label for="trackingNumber{index}" class="mb-2">
 						<div class="text-white">Kit {index + 1}</div>
 					</Label>
-					<button type="button" tabindex="-1" class="text-white order-last cursor-pointer" on:click={() => {deleteParcel(index)}}>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+					<button
+						type="button"
+						tabindex="-1"
+						class="text-white order-last cursor-pointer"
+						on:click={() => {
+							deleteParcel(index);
+						}}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
 							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 						</svg>
 					</button>
@@ -226,7 +255,9 @@
 					tabindex="-1"
 					bind:value={trackingNumber[index]}
 					placeholder="Tracking number"
-					on:input={() => {addParcel(index)}}
+					on:input={() => {
+						addKit(index);
+					}}
 				/>
 				<Input
 					type="text"
