a0ab596e2d625b7140d8dcd39bea018fc9b95b0c
Trevor <trevor@quanta-inc.com>
2023-02-02 13:11:07 -0500

    fix: site manifest

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA({
-			manifest: {
-				short_name: 'QTrack',
-				name: 'Quanta Track',
-				start_url: '/',
-				scope: '/',
-				display: 'standalone',
-				theme_color: '#FFCB05',
-				background_color: '#00274C',
-				icons: [
-					{
-						src: '/logos/pwa-192-192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/logos/pwa-512-512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					},
-					{
-						src: '/logos/pwa-512-512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable'
-					}
-				]
-			},
-		})
 	]
 };
 
